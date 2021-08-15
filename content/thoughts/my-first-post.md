a60109306b566af752056b2d8d64df3abba8efe8
tomouellette <t.ouellette@mail.utoronto.ca>
2021-08-14 20:33:38 -0400

    Updating

--- content/thoughts/my-first-post.md
+++
@@ -1,5 +0,0 @@
----
-title: "Coming soon"
-date: 2021-08-13T15:30:35-04:00
-draft: true
----
--- push_site.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd public
-git init
-git remote add origin https://github.com/tomouellette/tomouellette.github.io.git
-git add .
-git commit -m "Deploying"
-git push --set-upstream origin master
